09559693d4cc6c6cf717201dd8f59b4041981d34
everysick <s.wakeup31@gmail.com>
2016-12-09 19:13:45 +0900

    Implement source interpreter

--- src/source.ml
+++ src/source.ml
@@ -1,61 +1,38 @@
 (* source.ml *)
 
-open Util
+type ident = string
 
-module SourceVal : Value = struct
-  type t =
-    | IntVal  of int
-    | BoolVal  of bool
-    | LamVal  of string * exp * env
-    | RecFunVal of string * string * exp * Env.value
+module type VARIABLE = sig
+  type t
+  val equal   : t * t -> bool
+  val compare : t * t -> int
 end
 
-module SourceVar : Variable = struct
+module SourceVar : VARIABLE = struct
   type t = ident
   let equal (x, x') = x = x'
   let compare = fun (a,b) -> String.compare a b
 end
 
-module SourceEnv : Env = struct
-  type key = SourceVar.t
-  type value = SourceVar.t
-
-  let empty = []
-
-  let lookup env x =
-    match env with
-    | [] -> failwith ("unbound variable: " ^ x)
-    | (y,v)::tl -> if x=y then v
-      else lookup x tl
-
-  let insert env x v = (x,v) :: env ;;
-end
-
-module SourceTypeVal : Value = struct
-  type t = TBool | TInt | TFunc | TApp
-end
-
-module SourceTypeVar : Variable = struct
+module SourceTypeVar : VARIABLE = struct
   include SourceVar
 end
 
-module SourceTypeEnv : Env = struct
-  type key = SourceTypeVar.t
-  type value = SourceTypeVal.t
+module type INTERPRETER = sig
+  type exp
+  type value
+  type ty
 
-  let empty = []
-
-  let lookup env x =
-    match env with
-    | [] -> failwith ("unbound variable: " ^ x)
-    | (y,v)::tl -> if x=y then v
-      else lookup x tl
-
-  let insert env x v = (x,v) :: env ;;
+  val type_check : exp -> bool
+  val type_inf   : exp -> ty
+  val eval       : exp -> value
 end
 
-module SourceExp : Exp = struct
-  type t =
+module SourceInterpreter : INTERPRETER = struct
+  module Env = Map.Make(String) ;;
+  module TypeEnv = Map.Make(String) ;;
+
+  type exp =
     | IntLit of int
     | BoolLit of bool
     | Var of string
@@ -72,76 +49,84 @@ module SourceExp : Exp = struct
     | Call of exp * exp
     | Lam of string * exp ;;
 
-  type ty = SourceTypeVal.t
-  type value = SourceVal.t
+  type ty = TBool | TInt | TFunc | TApp
+
+  type value =
+    | IntVal  of int
+    | BoolVal  of bool
+    | LamVal  of string * exp * value Env.t
+    | RecFunVal of string * string * exp * value Env.t
 
   let type_check exp =
     let rec walk env e =
       match exp with
       | _ -> failwith "wrong value"
     in
-    walk SourceTypeEnv.empty exp ;;
+    walk TypeEnv.empty exp ;;
 
   let type_inf exp =
     match exp with
     | _ -> failwith "wrong value" ;;
 
-  let rec eval e env =
-    let binop f e1 e2 env =
-      let eval_e2 = eval e2 env in
-      let eval_e1 = eval e1 env in
-      match (eval_e1, eval_e2) with
-      | (IntVal(n1),IntVal(n2)) -> IntVal(f n1 n2)
-      | _ -> failwith "integer value expected"
+  let  eval exp =
+    let rec walk e env =
+      let binop f e1 e2 =
+        let walk_e2 = walk e2 env in
+        let walk_e1 = walk e1 env in
+        match (walk_e1, walk_e2) with
+        | (IntVal(n1),IntVal(n2)) -> IntVal(f n1 n2)
+        | _ -> failwith "integer value expected"
+      in
+      let equal_val e1 e2 =
+        let walk_e2 = walk e2 env in
+        let walk_e1 = walk e1 env in
+        match (walk_e1, walk_e2) with
+        | (IntVal(n1),IntVal(n2)) -> n1 = n2
+        | (BoolVal(b1),BoolVal(b2)) -> b1 = b2
+        | _ -> failwith "wrong value"
+      in
+      let greater_val e1 e2 =
+        let walk_e2 = walk e2 env in
+        let walk_e1 = walk e1 env in
+        match (walk_e1, walk_e2) with
+        | (IntVal(n1),IntVal(n2)) -> n1 > n2
+        | (BoolVal(b1),BoolVal(b2)) -> b1 > b2
+        | _ -> failwith "wrong value"
+      in
+      match e with
+      | Var(x)            -> Env.find x env
+      | IntLit(n)         -> IntVal(n)
+      | BoolLit(b)        -> BoolVal(b)
+      | Lam(x,e1)         -> LamVal(x, e1, env)
+      | Call(e1,e2)       ->
+         let arg = (walk e2 env) in
+         let func = (walk e1 env) in
+         (match func with
+         | LamVal(x,body,env1) ->
+            let env2 = Env.add x arg env1 in
+            walk body env2
+         | RecFunVal(f,x,body,env1) ->
+            let env2 = Env.add f func (Env.add x arg env1) in
+            walk body env2
+         | _ -> failwith "wrong value in App")
+      | Plus(e1,e2)       -> binop ( + ) e1 e2
+      | Times(e1,e2)      -> binop ( * ) e1 e2
+      | Minus(e1,e2)      -> binop ( - ) e1 e2
+      | Div(e1,e2)        ->
+         binop (fun p1 p2 ->
+           match p2 with
+           | 0 -> failwith "Divide by zero exception"
+           | _ -> p1 / p2 ) e1 e2
+      | Eq(e1,e2)         -> BoolVal(equal_val e1 e2)
+      | NotEq(e1,e2)      -> BoolVal(not (equal_val e1 e2))
+      | Greater(e1,e2)    -> BoolVal(greater_val e1 e2)
+      | Less(e1,e2)       -> BoolVal(not (greater_val e1 e2))
+      | Let(x,e1,e2)      ->
+         let env1 = Env.add x (walk e1 env) env in
+         walk e2 env1
+      | LetRec(f,x,e1,e2) ->
+         let env1 = Env.add f (RecFunVal (f, x, e1, env)) env in
+         walk e2 env1
     in
-    let equal_val e1 e2 =
-      let eval_e2 = eval e2 env in
-      let eval_e1 = eval e1 env in
-      match (eval_e1, eval_e2) with
-      | (IntVal(n1),IntVal(n2)) -> n1 = n2
-      | (BoolVal(b1),BoolVal(b2)) -> b1 = b2
-      | _ -> failwith "wrong value"
-    in
-    let greater_val e1 e2 =
-      let eval_e2 = eval e2 env in
-      let eval_e1 = eval e1 env in
-      match (eval_e1, eval_e2) with
-      | (IntVal(n1),IntVal(n2)) -> n1 > n2
-      | (BoolVal(b1),BoolVal(b2)) -> b1 > b2
-      | _ -> failwith "wrong value"
-    in
-    match e with
-    | Var(x)            -> lookup x env
-    | IntLit(n)         -> IntVal(n)
-    | BoolLit(b)        -> BoolVal(b)
-    | Lam(x,e1)         -> LamVal(x, e1, env)
-    | Call(e1,e2)        ->
-       let arg = (eval e2 env) in
-       let func = (eval e1 env) in
-       (match func with
-       | LamVal(x,body,env1) ->
-          let env2 = (ext env1 x arg) in
-          eval body env2
-       | RecFunVal(f,x,body,env1) ->
-          let env2 = (ext (ext env1 x arg) f func) in
-          eval body env2
-       | _ -> failwith "wrong value in App")
-    | Plus(e1,e2)       -> binop ( + ) e1 e2 env
-    | Times(e1,e2)      -> binop ( * ) e1 e2 env
-    | Minus(e1,e2)      -> binop ( - ) e1 e2 env
-    | Div(e1,e2)        ->
-       binop (fun p1 p2 ->
-         match p2 with
-         | 0 -> failwith "Divide by zero exception"
-         | _ -> p1 / p2 ) e1 e2 env
-    | Eq(e1,e2)         -> BoolVal(equal_val e1 e2)
-    | NotEq(e1,e2)      -> BoolVal(not (equal_val e1 e2))
-    | Greater(e1,e2)    -> BoolVal(greater_val e1 e2)
-    | Less(e1,e2)       -> BoolVal(not (greater_val e1 e2))
-    | Let(x,e1,e2)      ->
-       let env1 = ext env x (eval e1 env) in
-       eval e2 env1
-    | LetRec(f,x,e1,e2) ->
-       let env1 = ext env f (RecFunVal (f, x, e1, env))
-       in eval e2 env1 ;;
+    walk exp Env.empty ;;
 end
--- src/target.ml
+++ src/target.ml
@@ -1,64 +1,64 @@
-(*
-  Util env module
-*)
+(* (\* *)
+(*   Util env module *)
+(* *\) *)
 
-type 'v env = (string * 'v) list ;;
+(* type 'v env = (string * 'v) list ;; *)
 
-let rec lookup env x =
-  match env with
-  | []        -> failwith (x ^ " not found")
-  | (y, v)::r -> if x=y then v else lookup r x;;
+(* let rec lookup env x = *)
+(*   match env with *)
+(*   | []        -> failwith (x ^ " not found") *)
+(*   | (y, v)::r -> if x=y then v else lookup r x;; *)
 
-(*
-  Runtime environment set.
+(* (\* *)
+(*   Runtime environment set. *)
 
-  - region is finite map: offsets ->  strable values
-  - strable value is (int or closure or region function closure)
+(*   - region is finite map: offsets ->  strable values *)
+(*   - strable value is (int or closure or region function closure) *)
 
-  - variable environment is finite map: program variables -> values
-  - region environment is is finite map: region variables -> region names
+(*   - variable environment is finite map: program variables -> values *)
+(*   - region environment is is finite map: region variables -> region names *)
 
-  - address is a pair (region name, offset)
-  - store is finite map: region names -> regions
-  - value is an address.
-*)
+(*   - address is a pair (region name, offset) *)
+(*   - store is finite map: region names -> regions *)
+(*   - value is an address. *)
+(* *\) *)
 
-type offset = int ;;
-type region_var = string ;;
-type region_name = string ;;
-type address = region_name * offset ;;
-type value = address ;;
+(* type offset = int ;; *)
+(* type region_var = string ;; *)
+(* type region_name = string ;; *)
+(* type address = region_name * offset ;; *)
+(* type value = address ;; *)
 
-type var_env = value env ;;
-type reg_env = region_name env ;;
+(* type var_env = value env ;; *)
+(* type reg_env = region_name env ;; *)
 
-type strable_value =
-  | Int of int
-  | Closure of string * expr * var_env * reg_env
-  | RegClosure of string * expr * var_env * reg_env ;;
+(* type strable_value = *)
+(*   | Int of int *)
+(*   | Closure of string * expr * var_env * reg_env *)
+(*   | RegClosure of string * expr * var_env * reg_env ;; *)
 
-type region = strable_value env ;;
-type store = region env ;;
+(* type region = strable_value env ;; *)
+(* type store = region env ;; *)
 
-(*
-  Grammer of region based meta language.
+(* (\* *)
+(*   Grammer of region based meta language. *)
 
-  TODO: Lexer
-*)
-type expr =
-  | IntLit of int * string                                       (* c at rho *)
-  | Var of string                                                (* x *)
-  | Prim of string * expr * expr * string                        (* x + y at rho *)
-  | Let of string * expr * expr                                  (* let x = e1 in e2 *)
-  | Letrec of string * string list * string * string expr * expr (* letrec f[rho1, rho2](x) at rho = e1 in e2*)
-  | Letreg of string list * expr                                 (* letregion rho1, rho2 in e *)
-  | Call of expr * expr                                          (* e1 e2 # Apply *)
-  | Lam of string * expr * string ;;                             (* lambda x.e at rho *)
+(*   TODO: Lexer *)
+(* *\) *)
+(* type expr = *)
+(*   | IntLit of int * string                                       (\* c at rho *\) *)
+(*   | Var of string                                                (\* x *\) *)
+(*   | Prim of string * expr * expr * string                        (\* x + y at rho *\) *)
+(*   | Let of string * expr * expr                                  (\* let x = e1 in e2 *\) *)
+(*   | Letrec of string * string list * string * string expr * expr (\* letrec f[rho1, rho2](x) at rho = e1 in e2*\) *)
+(*   | Letreg of string list * expr                                 (\* letregion rho1, rho2 in e *\) *)
+(*   | Call of expr * expr                                          (\* e1 e2 # Apply *\) *)
+(*   | Lam of string * expr * string ;;                             (\* lambda x.e at rho *\) *)
 
-(*
-  evaluate
-*)
+(* (\* *)
+(*   evaluate *)
+(* *\) *)
 
-let rec evaluate (e: expr) (s: store) (ve: var_env) (re: reg_env) =
-  match e with
-  | _ -> failwith "unkown type expr" ;;
+(* let rec evaluate (e: expr) (s: store) (ve: var_env) (re: reg_env) = *)
+(*   match e with *)
+(*   | _ -> failwith "unkown type expr" ;; *)
