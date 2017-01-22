9dbe898e6064912df121e05b53586f91dd72dc80
everysick <s.wakeup31@gmail.com>
2017-01-22 13:52:26 +0900

    [WIP] impl target language

--- src/source.ml
+++ src/source.ml
@@ -48,6 +48,7 @@ end
 module SourceInterpreter : INTERPRETER = struct
   module Env = Map.Make(String) ;;
   module TypeEnv = Map.Make(String) ;;
+  module TypeSubst = Map.Make(String) ;;
 
   type ty = TBool | TInt | TArrow of ty * ty
 
@@ -57,11 +58,156 @@ module SourceInterpreter : INTERPRETER = struct
     | LamVal  of ident * exp * value Env.t
     | RecFunVal of ident * ident * exp * value Env.t
 
-  let type_inf exp =
-    match exp with
-    | _ -> failwith "wrong value" ;;
+  let new_typevar n =
+    (TVar ("'a" ^ (string_of_int n)), n+1)
 
-  let  eval exp =
+  (* 型環境 te1 における 型変数t1 を TInt に変更した型環境 *)
+  let rec substitute tvar t te =
+    List.map (fun (x,t2) -> if t2 = tvar then (x,t) else (x,t2)) te
+
+  let rec substitute tvar t te =
+    TypeEnv.mapi (fun (k,v) -> if v = tvar then (k,t) else (k,v)) te
+
+  let rec occurs tx t =
+    if tx = t then true
+    else
+      match t with
+      | TArrow(t1,t2) -> (occurs tx t1) || (occurs tx t2)
+      | _ -> false
+
+  (* subst_ty : tysubst -> ty -> ty *)
+  (* 代入thetaを型t に適用する *)
+  let rec subst_ty theta t =
+    let rec subst_ty1 theta1 s =
+      match theta1 with
+      |	[] -> TVar(s)
+      | (tx,t1):: theta2 ->
+	       if tx = s then t1
+	       else subst_ty1 theta2 s
+    in match t with
+    |  TInt -> TInt
+    | TBool -> TBool
+    | TArrow(t2,t3) -> TArrow(subst_ty theta t2, subst_ty theta t3)
+    | TVar(s) -> subst_ty1 theta s
+
+  let rec apply_ty theta t =
+    match t with
+    | TInt -> TInt
+    | TBool -> TBool
+    | TArrow(t1, t2) -> TArrow(apply_ty theta t1, apply_ty theta t2)
+    | TVar(s) ->
+       if TypeSubst.exists (fun k v -> k = s) theta
+       then TypeSubst.find s theta
+       else TVar(s)
+
+  (* subst_tyenv  : tysubst -> tyenv -> tyenv *)
+  (* 代入thetaを型環境 te に適用する *)
+  let subst_tyenv theta te =
+    List.map (fun (x,t) -> (x, subst_ty theta t)) te
+  (* List.mapは OCaml における リストに対する iterator である *)
+
+  let apply_tyenv theta te =
+    TypeEnv.map (fun v -> apply_ty theta v) te
+
+  (* subst_eq : tysubst -> (ty * ty) list -> (ty * ty) list *)
+  (* 代入thetaを型の等式のリスト eql に適用する *)
+  let subst_eql theta eql =
+    List.map (fun (t1,t2) -> (subst_ty theta t1, subst_ty theta t2))
+	    eql
+
+  let apply_eql theta eql =
+    List.map (fun (t1,t2) -> (apply_ty theta t1, apply_ty theta t2)) eql
+
+  (* compose_subst : tysubst -> tysubst -> tysubst *)
+  (* 2つの代入を合成した代入を返す。theta1 が「先」でtheta2が「後」である *)
+  let rec compose_subst theta2 theta1 =
+    (*各要素は適用したものを入れる*)
+    let theta11 =
+      List.map (fun (tx,t) -> (tx, subst_ty theta2 t)) theta1
+    in
+    (* 無いものを入れる *)
+    List.fold_left (fun tau -> fun (tx,t) ->
+		  try
+			  let _ = lookup tx theta1 in
+			  tau
+		  with Failure(_) ->
+			  (tx,t) :: tau)
+      theta11
+      theta2
+
+  let rec merge_subst theta2 theta1 =
+    TypeSubst.merge (fun k t1 t2 ->
+      match (t1, t2)z with
+      | (Some x, Some y)
+      | (Some x, None  ) -> apply_ty theta2 x
+      | (None  , Some x) -> x
+    ) thenta1 theta2
+
+  (* unify : Map.bindings -> tysubst -> tysubst *)
+  let unify eql =
+    let rec solve eql theta =
+      match eql with
+      | [] -> theta
+      | (t1,t2):: eql2 ->
+	       if t1 = t2 then solve eql2 theta
+	       else
+           begin
+             match (t1,t2) with
+	           | (TArrow(t11,t12),TArrow(t21,t22))
+	             -> solve ((t11,t21)::(t12,t22)::eql2) theta
+	           | (TVar(s), _)
+	             -> if (occurs t1 t2) then failwith "unification failed"
+	               else solve (subst_eql [(s,t2)] eql2)
+	                 (compose_subst [(s,t2)] theta)
+	           | (_,TVar(s))
+	             -> if (occurs t2 t1) then failwith "unification failed"
+	               else solve (subst_eql [(s,t1)] eql2)
+	                 (compose_subst [(s,t1)] theta)
+	           | (_,_) -> failwith "unification failed"
+           end
+    in solve eql TypeSubst.empty
+
+  let type_inf te e n =
+    match e with
+    | Var(s) ->
+	     (try
+	        let t1 = TypeEnv.find s te in (te, t1, theta0, n)
+	      with Failure(_) ->
+	        let (tx,n1) = new_typevar n in
+	        let te1 = TypeEnv.add s tx te in
+	        (te1, tx, theta0, n1))
+    | IntLit(_)   -> (te, TInt, theta0, n)
+    | BoolLit(_)  -> (te, TBool, theta0, n)
+    | Plus(e1,e2) ->
+	     let (te1, t1, theta1, n1) = tinf te e1 n in
+	     let (te2, t2, theta2, n2) = tinf te1 e2 n1 in
+	     let t11 = subst_ty theta2 t1 in
+	     let theta3 = unify [(t11,TInt); (t2,TInt)] in
+	     let te3 = subst_tyenv theta3 te2 in
+	     let theta4 = compose_subst theta3
+	       (compose_subst theta2 theta1) in
+	     (te3, TInt, theta4, n2)
+    | Fun(x,e) ->
+	     let (tx,n1) = new_typevar n in
+	     let te1 = TypeEnv.add x tx te in
+	     let (te2, t1, theta1, n2) = tinf te1 e n1 in
+	     let t2 = subst_ty theta1 tx in
+	     let te3 = TypeEnv.remove x te2 in
+	     (te3, TArrow(t2, t1), theta1, n2)
+    | App(e1,e2) ->
+	     let (te1, t1, theta1, n1) = tinf te e1 n in
+	     let (te2, t2, theta2, n2) = tinf te1 e2 n1 in
+	     let (tx,n3) = new_typevar n2 in
+	     let t11 = subst_ty theta2 t1 in
+	     let theta3 = unify [(t11,TArrow(t2,tx))] in
+	     let t3 = subst_ty theta3 tx in
+	     let te3 = subst_tyenv theta3 te2 in
+	     let theta4 = compose_subst theta3
+	       (compose_subst theta2 theta1) in
+	     (te3, t3, theta4, n3)
+    | _ -> failwith "unknown expression"
+
+  let eval exp =
     let rec walk e env =
       let binop f e1 e2 =
         let walk_e2 = walk e2 env in
--- src/target.ml
+++ src/target.ml
@@ -1,64 +1,208 @@
-(* (\* *)
-(*   Util env module *)
-(* *\) *)
+type ident = string ;;
 
-(* type 'v env = (string * 'v) list ;; *)
+type exp =
+  | IntLit of int
+  | Var of ident
+  | Let of ident * exp * exp
+  | LetReg of ident * exp
+  | LetRec of ident * ident * exp * exp
+  | Call of exp * exp
+  | Lam of ident * exp
+  | Empty ;;
 
-(* let rec lookup env x = *)
-(*   match env with *)
-(*   | []        -> failwith (x ^ " not found") *)
-(*   | (y, v)::r -> if x=y then v else lookup r x;; *)
+module type VARIABLE = sig
+  type t
+  val intro   : ident -> t
+  val equal   : t * t -> bool
+  val compare : t * t -> int
+end
 
-(* (\* *)
-(*   Runtime environment set. *)
+module TyVar : VARIABLE = struct
+  type t = ident
+  let intro x = x
+  let equal (x, x') = x = x'
+  let compare = fun (a,b) -> String.compare a b
+  let new_var c = intro ("'t" ^ (string_of_int c))
+end
 
-(*   - region is finite map: offsets ->  strable values *)
-(*   - strable value is (int or closure or region function closure) *)
+module TyVarSet = Set.Make(TyVar.t) ;;
 
-(*   - variable environment is finite map: program variables -> values *)
-(*   - region environment is is finite map: region variables -> region names *)
+module RegVar : VARIABLE = struct
+  type t = ident
+  let intro x = x
+  let equal (x, x') = x = x'
+  let compare = fun (a,b) -> String.compare a b
+  let new_var c = intro ("'r" ^ (string_of_int c))
+end
 
-(*   - address is a pair (region name, offset) *)
-(*   - store is finite map: region names -> regions *)
-(*   - value is an address. *)
-(* *\) *)
+module RegVarSet = Set.Make(RegVar.t) ;;
 
-(* type offset = int ;; *)
-(* type region_var = string ;; *)
-(* type region_name = string ;; *)
-(* type address = region_name * offset ;; *)
-(* type value = address ;; *)
+module EffVar : VARIABLE = struct
+  type t = ident
+  let intro x = x
+  let equal (x, x') = x = x'
+  let compare = fun (a,b) -> String.compare a b
+  let new_var c = intro ("'e" ^ (string_of_int c))
+end
 
-(* type var_env = value env ;; *)
-(* type reg_env = region_name env ;; *)
+module EffVarSet = Set.Make(EffVar.t) ;;
 
-(* type strable_value = *)
-(*   | Int of int *)
-(*   | Closure of string * expr * var_env * reg_env *)
-(*   | RegClosure of string * expr * var_env * reg_env ;; *)
+module VarStream = struct
+  let intro = (0, 0, 0)
+  let fresh_type_var (a, b, c) =
+    (TypeVar.new_var a, (a+1, b, c))
+  let fresh_reg_var (a, b, c) =
+    (RegVar.new_var b, (a, b+1, c))
+  let fresh_eff_var (a, b, c) =
+    (EffVar.new_var c, (a, b, c+1))
+end
 
-(* type region = strable_value env ;; *)
-(* type store = region env ;; *)
+(* atomic_effect * effect var *)
+type effect =
+  | EffVar of EffVar.t
+  | EffPut of RegVar.t
+  | EffGet of RegVar.t
 
-(* (\* *)
-(*   Grammer of region based meta language. *)
+module EffSet = Set.Make(effect) ;;
 
-(*   TODO: Lexer *)
-(* *\) *)
-(* type expr = *)
-(*   | IntLit of int * string                                       (\* c at rho *\) *)
-(*   | Var of string                                                (\* x *\) *)
-(*   | Prim of string * expr * expr * string                        (\* x + y at rho *\) *)
-(*   | Let of string * expr * expr                                  (\* let x = e1 in e2 *\) *)
-(*   | Letrec of string * string list * string * string expr * expr (\* letrec f[rho1, rho2](x) at rho = e1 in e2*\) *)
-(*   | Letreg of string list * expr                                 (\* letregion rho1, rho2 in e *\) *)
-(*   | Call of expr * expr                                          (\* e1 e2 # Apply *\) *)
-(*   | Lam of string * expr * string ;;                             (\* lambda x.e at rho *\) *)
+(* effect var * effect *)
+module ArrowEff = struct
+  type t = EffVar.t * EffSet.t
 
-(* (\* *)
-(*   evaluate *)
-(* *\) *)
+  let frv (_, phi) =
+    RegSet.fold (function
+    | (EffPut(r), set) -> RegSet.add r set
+    | (EffGet(r), set) -> RegSet.add r set
+    | (_, set) -> set
+    ) RegSet.empty phi
 
-(* let rec evaluate (e: expr) (s: store) (ve: var_env) (re: reg_env) = *)
-(*   match e with *)
-(*   | _ -> failwith "unkown type expr" ;; *)
+  let fev (eps, phi) =
+    EffSet.union
+      (EffSet.singleton eps)
+      (
+        EffSet.fold (function
+        | (EffVar(s), set) -> EffSet.add s set
+        | (_, set) -> set
+        ) EffSet.empty phi
+      )
+end
+
+type
+  target_ty =
+  | TVar of TypeVar.t
+  | TInt
+  | TBool
+  | TArrow of ty_with_place * ArrowEff.t * ty_with_place
+and
+  ty_with_place = target_ty * RegVar.t (* type * region var *)
+
+module AnnotatedType = struct
+  type t = ty_with_place * EffectSet.t
+
+  let fresh_annnotated_ty vs = ;;
+
+  let rec ftv ty = match ty with
+    | (TVar(s), _) -> TyVarSet.singleton s
+    | (TArrow(t1, (ev, eff), t2), _) -> TyVarSet.union (ftv t1) (ftv t2)
+    | (_, _) -> TyVarSet.empty ;;
+
+  let rec frv ty = match ty with
+    | (TArrow(t1, (ev, eff), t2), r) ->
+       RegVarSet.union
+         (RegVarSet.union (frv t1) (frv t2))
+         (RegVarSet.union (RegVarSet.singleton r) (ArrowEff.frv eff))
+    | (_, r) -> RegVarSet.singleton r ;;
+
+  let rec fev ty = match ty with
+    | (TArrow(t1, (ev, eff), t2), _) ->
+       EffVarSet.union
+         (EffVarSet.union (fev t1) (fev t2))
+         (EffSet.union (EffVarSet.singleton ev) (ArrowEff.fev eff))
+    | (_, _) -> EffVarSet.empty ;;
+
+  let fv ty = ((ftv ty), (frv ty), (fev ty))
+
+  let unify = failwith 0
+end
+
+module TypedExp = struct
+  type t = exp * AnnotatedType.t
+end
+
+module EffVarMap = Map.Make(EffVar.t)
+module RegVarMap = Map.Make(RegVar.t)
+module TyVarMap = Map.Make(TyVar.t)
+
+module Substitute = struct
+  let compose s1 s2 = failwith 0 ;;
+
+  let subst_reg ((st, sr, se), r) =
+    match RegVarMap.find r sr with
+    | None -> r
+    | Some r' = r'
+
+  let subst_eff ((st, sr, se) as s, eff) =
+    EffSet.fold (function
+    | (EffPut(r), set) -> EffSet.add (EffPut(subst_reg s r)) set
+    | (EffGet(r), set) -> EffSet.add (EffGet(subst_reg s r)) set
+    | (EffVar(x), set) -> EffSet.add (
+      EffVar(match EffVarMap.find x se with
+      | None -> x
+      | Some x' -> x')
+    ) set) EffSet.empty eff
+
+  let subst_arrow_eff ((st, sr, se) as s, (eps, eff)) =
+    let (eps1, eff1) =
+      match EffVarMap.find eps se with
+      | None -> (eps, eff)
+      | Some (eps', eff') -> (eps', eff') in
+    (eps1, subst_eff s eff1)
+
+  let rec subst_ty ((st, sr, se) as s, t) =
+    match t with
+    | TInt -> TInt
+    | TBool -> TBool
+    | TVar(x) ->
+       (match TyVarMap.find x st with
+       | None -> t
+       | Some x' -> x')
+    | TArrow((t1, r1), ae, (t2, r2)) ->
+       TArrow(
+         ((subst_ty s t1), (subst_reg s r1)),
+         subst_arrow_eff s ae,
+         ((subst_ty s t2), (subst_reg s r2))
+       )
+
+  let rec subst (s, (t, r)) = ((subst_ty s t), (subst_reg s r))
+end
+
+module IndentEnv = Map.Make(String) ;;
+
+module type TRANSLATOR = sig
+  val translate : exp -> TypedExp.t
+end
+
+module TargetTranslator = struct
+  (* translate: exp -> TypedExp.t *)
+  let translate exp =
+    let unify = 1 in
+    let rec walk e env =
+      match e with
+      | IntLit(n) ->
+         let rv = RegVar.new_var in
+	       (IntLit(n), (int, rv), put(rv))
+      | Var(s) ->
+         let mu = TypeEnv.find s env in
+         (Var(s), mu, [])
+      | Call(e1,e2) ->
+      | Lam(x,e1) ->
+      | Let(x,e1,e2) ->
+         let (_,twp1,ef1) = walk e1 env in
+         let ex_env = TypeEnv.add x twp1 env in
+         let (_,twp2,ef2) = walk e2 ex_env in
+         (Let(x, e1, e2), twp2, ef1 @ ef2)
+      | LetRec(f,x,e1,e2) -> failwith "unsupported let rec"
+      | _ -> failwith "unknown expression"
+    in
+    walk TypeEnv.empty ;;
+end
